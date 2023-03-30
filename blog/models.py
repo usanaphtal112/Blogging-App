4b9927a626200adef6bbb34c8afd492f5d12bbab
usanaphtal112 <usanaphtal112@gmail.com>
2023-03-30 11:53:14 +0200

    blog posts added

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -11,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -25,6 +31,9 @@ class Post(models.Model):
         max_length=2, choices=Status.choices, default=Status.DRAFT
     )
 
+    objects = models.Manager()  # The deafault manager
+    published = PublishedManager()  # Custom manager created above
+
     class Meta:
         ordering = ["-publish"]
         indexes = [
@@ -33,3 +42,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
