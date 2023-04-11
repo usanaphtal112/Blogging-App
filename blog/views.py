f2e4a3b7c16ff351a551ef3e7910d8a97ec40196
usanaphtal112 <usanaphtal112@gmail.com>
2023-04-11 19:25:17 +0200

    Added feeds, Search, ..

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,13 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 # class PostListView(ListView):
@@ -119,4 +121,38 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            search_vector = SearchVector(
+                "title", config="spanish", weight="A"
+            ) + SearchVector("body", weight="B")
+            search_query = SearchQuery(query, config="spanish")
+            results = (
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                )
+                .filter(rank__gte=0.3)
+                .order_by("-rank")
+            )
+
+            # results = (
+            #     Post.published.annotate(
+            #         similarity=TrigramSimilarity("title", query),
+            #     )
+            #     .filter(similarity__gt=0.1)
+            #     .order_by("-similarity")
+            # )
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
